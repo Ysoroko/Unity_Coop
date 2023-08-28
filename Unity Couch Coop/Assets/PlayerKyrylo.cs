4544c1b766b7f8744f2bf498d5126c4862603fbf
Yaroslav Soroko (yaso) <yaso@odoo.com>
2023-08-28 19:10:51 +0200

    fixed main, deleted extra scripts

--- Unity Couch Coop/Assets/PlayerKyrylo.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerKyrylo : PlayerParent
-{
-    // Start is called before the first frame update
-       
-}
