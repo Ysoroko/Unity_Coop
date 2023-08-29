f7bd28f78a767aac2971053677ac7269b6eced76
Yaroslav Soroko (yaso) <yaso@odoo.com>
2023-08-29 23:37:21 +0200

    working all

--- Unity Couch Coop/Assets/PlayerScripts/ResetPlayer.cs
+++ Unity Couch Coop/Assets/PlayerScripts/ResetPlayer.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class ResetPlayer : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    public Rigidbody2D rb;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
     void OnBecameInvisible()
     {
