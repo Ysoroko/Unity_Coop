36965ede7f1a7858ea2a81e74e63b1b9b13e6d87
DimaSoroko <disoroko@gmail.com>
2023-08-29 21:42:04 +0200

    pickups finished

--- Unity Couch Coop/Assets/Pickup.cs
+++ Unity Couch Coop/Assets/Pickup.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Pickup : MonoBehaviour
 {
-
+    public GameObject weapon;
+    public GameObject[] weapons;
     void Start()
     {
-        Destroy(gameObject, 1f);
+        weapon = weapons[Random.Range(0, weapons.Length)];
+        Destroy(gameObject, 500f);
     }
     
 
       public void OnTriggerEnter2D(Collider2D other)
     {
-        
+         other.gameObject.GetComponent<Weapon>().weaponPrefab = weapon;
         if(other.tag == "Player")
             Destroy(gameObject);
     }
